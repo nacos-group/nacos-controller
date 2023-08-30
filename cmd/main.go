9482889af01d6cd29b858a244bb9b9b5750a17c5
之卫 <panjunfeng.pjf@alibaba-inc.com>
2023-08-30 19:21:39 +0800

    init webhook

--- cmd/main.go
+++ cmd/main.go
@@ -96,6 +96,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "DynamicConfiguration")
 		os.Exit(1)
 	}
+	if err = (&nacosiov1.DynamicConfiguration{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "DynamicConfiguration")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
