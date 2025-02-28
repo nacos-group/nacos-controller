398baa12954acce40f095f147b451eddd28745cc
Sunrisea <1336317033@qq.com>
2025-02-28 16:23:46 +0800

    add tag of config publish

--- cmd/main.go
+++ cmd/main.go
@@ -60,8 +60,8 @@ func main() {
 	var enableLeaderElection bool
 	var probeAddr string
 	var enableWebhook bool
-	flag.StringVar(&metricsAddr, "metrics-bind-address", ":7847", "The address the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":7846", "The address the probe endpoint binds to.")
+	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
